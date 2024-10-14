c364697667a28a915e4172ae0c3e7ec6744ff2d0
Ashwin <ashwinparthiban26@gmail.com>
2024-10-14 15:12:45 +0530

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -18,7 +18,7 @@ const Main = () => {
             {!showResult
             ?<>
              <div className="greet">
-              <p><span>Hello, Dev.</span></p>
+              <p><span>Hello, Ashwin.</span></p>
               <p>How can I help you today?</p>
             </div>
             <div className="cards">
